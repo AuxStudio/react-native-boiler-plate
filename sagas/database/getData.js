53152da25fc41603c728a185da2573db3da7ef28
Shaun Saker <shaun@aux.co.za>
2018-04-28 08:03:17 +0200

    Update sagas/database

---
+++ sagas/database/getData.js
@@ -0,0 +1,32 @@
+import { call, put } from 'redux-saga/effects';
+
+import database from '../../services/database';
+
+export default function* getData(action) {
+  try {
+    const { payload, error } = yield call(database.getData);
+
+    if (__DEV__) {
+      console.log('getData', payload);
+    }
+
+    if (error) {
+      yield put({
+        type: 'SET_MESSAGE',
+        payload: new Error(payload),
+        error: true,
+      });
+    } else if (action.nextAction) {
+      yield put({
+        ...action.nextAction,
+        payload,
+      });
+    }
+  } catch (error) {
+    yield put({
+      type: 'SET_MESSAGE',
+      payload: new Error(error),
+      error: true,
+    });
+  }
+}
---
+++ sagas/database/updateData.js
@@ -0,0 +1,31 @@
+import { call, put } from 'redux-saga/effects';
+
+import database from '../../services/database';
+
+export default function* updateData(action) {
+  try {
+    const { payload, error } = yield call(database.updateData);
+
+    if (__DEV__) {
+      console.log('updateData', payload);
+    }
+
+    if (error) {
+      yield put({
+        type: 'SET_MESSAGE',
+        payload: new Error(payload),
+        error: true,
+      });
+    } else if (action.nextAction) {
+      yield put({
+        ...action.nextAction,
+      });
+    }
+  } catch (error) {
+    yield put({
+      type: 'SET_MESSAGE',
+      payload: new Error(error),
+      error: true,
+    });
+  }
+}
