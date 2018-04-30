b70b121e3274fafeece2683f843af1813e1748e8
Shaun Saker <shaun@aux.co.za>
2018-04-30 16:42:56 +0200

    Remove linkUserWithCredential service

--- services/auth/index.js
+++ services/auth/index.js
@@ -3,7 +3,6 @@ import signInUserAnonymously from './signInUserAnonymously';
 import getUserCredentialFromEmail from './getUserCredentialFromEmail';
 import getUserCredentialFromFacebook from './getUserCredentialFromFacebook';
 import getUserCredentialFromGoogle from './getUserCredentialFromGoogle';
-import linkUserWithCredential from './linkUserWithCredential';
 import signInUserWithCredential from './signInUserWithCredential';
 import sendPasswordResetEmail from './sendPasswordResetEmail';
 import signOutUser from './signOutUser';
@@ -14,7 +13,6 @@ module.exports = {
   getUserCredentialFromEmail,
   getUserCredentialFromFacebook,
   getUserCredentialFromGoogle,
-  linkUserWithCredential,
   signInUserWithCredential,
   sendPasswordResetEmail,
   signOutUser,
