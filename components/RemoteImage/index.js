3c16fc05765d31a8bf6e746f68bada5148a69f04
Shaun Saker <shaun@aux.co.za>
2018-05-28 17:53:44 +0200

    Components index is component

--- components/RemoteImage/index.js
+++ components/RemoteImage/index.js
@@ -1,3 +1,72 @@
-import RemoteImage from './RemoteImage';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, ViewPropTypes, Text, ActivityIndicator } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default RemoteImage;
+import styles from './styles';
+import styleConstants from '../../styleConstants';
+
+export default class RemoteImage extends React.Component {
+  static propTypes = {
+    source: PropTypes.shape({
+      uri: PropTypes.string,
+    }),
+    borderRadius: PropTypes.number,
+    iconStyle: Text.propTypes.style,
+    style: ViewPropTypes.style,
+    loaderColor: PropTypes.string,
+  };
+
+  static defaultProps = {
+    loaderColor: styleConstants.colors.primaryText,
+  };
+
+  state = {
+    isLoading: true,
+    hasError: false,
+  };
+
+  setError = () => {
+    this.setState({
+      hasError: true,
+    });
+  };
+
+  setLoading = (isLoading) => {
+    this.setState({
+      isLoading,
+    });
+  };
+
+  render() {
+    const borderRadiusStyles = this.props.borderRadius && { borderRadius: this.props.borderRadius };
+    let backgroundComponent;
+
+    if (this.state.hasError) {
+      backgroundComponent = (
+        <View style={[styles.backgroundContainer, borderRadiusStyles]}>
+          <Icon name="error-outline" style={[styles.icon, this.props.iconStyle]} />
+        </View>
+      );
+    } else if (this.state.isLoading) {
+      backgroundComponent = (
+        <View style={[styles.backgroundContainer, borderRadiusStyles]}>
+          <ActivityIndicator size="large" color={this.props.loaderColor} />
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <FastImage
+          source={this.props.source}
+          style={[styles.image, borderRadiusStyles, this.props.style]}
+          onLoadEnd={() => this.setLoading(false)}
+          onError={this.setError}
+        />
+        {backgroundComponent}
+      </View>
+    );
+  }
+}
