ad7fdc7619d3022701d14943452b7054c8503361
Shaun Saker <shaun@aux.co.za>
2018-06-03 07:55:49 +0200

    Update docs so that firebase config files are added to config/firebase - missed in #16

--- envscript.sh
+++ envscript.sh
@@ -3,10 +3,10 @@
 if [ "$ENV" == "production"  ];
 then
   echo "Switching to Firebase Production environment"
-  yes | cp -rf "firebase_environments/production/google-services.json" android/app
-  yes | cp -rf "firebase_environments/production/GoogleService-Info.plist" ios
+  yes | cp -rf "config/firebase/production/google-services.json" android/app
+  yes | cp -rf "config/firebase/production/GoogleService-Info.plist" ios
 else
   echo "Switching to Firebase Dev environment"
-  yes | cp -rf "firebase_environments/development/google-services.json" android/app
-  yes | cp -rf "firebase_environments/development/GoogleService-Info.plist" ios
+  yes | cp -rf "config/firebase/development/google-services.json" android/app
+  yes | cp -rf "config/firebase/development/GoogleService-Info.plist" ios
 fi
