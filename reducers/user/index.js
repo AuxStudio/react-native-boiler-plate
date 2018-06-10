99b7b0ace9f84d80895ab065ae0febcf9286fda5
Shaun Saker <shaun@aux.co.za>
2018-06-10 12:14:43 +0200

    Rename UPDATE_USER_PROFILE_DATA

--- reducers/user/index.js
+++ reducers/user/index.js
@@ -13,11 +13,13 @@ export default function userReducer(state = initialState, action = {}) {
       newState.authenticated = true;
       return newState;
 
-    case 'UPDATE_USER_PROFILE_DATA': {
+    case 'UPDATE_USER_DATA': {
       newState = utils.objects.cloneObject(state);
+
       // Get the ref from the key of the payload
       const refs = Object.keys(action.payload);
       const ref = refs[0];
+
       newState[ref] = action.payload[ref];
       return newState;
     }
