eee337306b35ad113572178893357cc153c44a4c
Shaun Saker <shaun@aux.co.za>
2018-06-10 15:11:54 +0200

    Move location services to their own folders

--- services/location/getDeviceLocation/index.js
+++ services/location/getDeviceLocation/index.js
@@ -1,4 +1,4 @@
-import utils from '../../utils';
+import utils from '../../../utils';
 
 export default function getDeviceLocation() {
   return new Promise((resolve, reject) => {
--- services/location/getFormattedAddressFormCoords/index.js
+++ services/location/getFormattedAddressFormCoords/index.js
@@ -1,5 +1,6 @@
 import Geocoder from 'react-native-geocoder';
-import utils from '../../utils';
+
+import utils from '../../../utils';
 
 export default function getFormattedAddressFromCoords(lat, lng) {
   return new Promise((resolve, reject) => {
