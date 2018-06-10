1b7f42226b85ce2ccb0bf73b8e8464584edb2b31
Shaun Saker <shaun@aux.co.za>
2018-06-10 12:01:32 +0200

    Add navigatinoReducer tests

---
+++ reducers/navigation/__tests__/navigationReducer.test.js
@@ -0,0 +1,13 @@
+jest.mock('react-native-router-flux', () => ({
+  ActionConst: 'ActionConst',
+}));
+
+/* eslint-disable import/first */
+// mock needs to be before imports
+import reducer from '../';
+import initialState from '../initialState';
+/* eslint-enable */
+
+it('should return the initial state', () => {
+  expect(reducer({ undefined }, {})).toEqual(initialState);
+});
