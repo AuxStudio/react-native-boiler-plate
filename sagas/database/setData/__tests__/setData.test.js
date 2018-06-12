6344a06e2e4912acee887d54618f147d6e2b3e90
Shaun Saker <shaun@aux.co.za>
2018-06-12 16:35:40 +0200

    SetData saga test

---
+++ sagas/database/setData/__tests__/setData.test.js
@@ -0,0 +1,126 @@
+import { call, put } from 'redux-saga/effects';
+import sagaHelper from 'redux-saga-testing';
+
+import utils from '../../../../utils';
+import setData from '../';
+
+const database = {
+  setData: jest.fn(),
+};
+
+const action = {
+  type: 'setData',
+  payload: {
+    ref: 'test',
+    data: {
+      foo: 'bar',
+    },
+  },
+};
+
+const nextAction = {
+  type: 'SUCCESS',
+};
+
+const actionWithNextAction = { ...action, meta: { nextAction } };
+
+const response = { foo: 'bar' };
+
+describe('When testing the saga without a nextAction and without a response from the api', () => {
+  const it = sagaHelper(setData(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(database.setData, action.payload.ref, action.payload.data)),
+    );
+  });
+
+  // Insert test for default nextAction (if any)
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga without a nextAction and with a response from the api', () => {
+  const it = sagaHelper(setData(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(database.setData, action.payload.ref, action.payload.data)),
+    );
+
+    return response;
+  });
+
+  // Insert test for default nextAction (if any)
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and without a response from the api', () => {
+  const it = sagaHelper(setData(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(database.setData, action.payload.ref, action.payload.data)),
+    );
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: {} }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and with a response from the api', () => {
+  const it = sagaHelper(setData(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(database.setData, action.payload.ref, action.payload.data)),
+    );
+
+    return response;
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: response }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga when an error is thrown from the api', () => {
+  const it = sagaHelper(setData(action));
+  const errorMessage = 'Something went wrong';
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(database.setData, action.payload.ref, action.payload.data)),
+    );
+
+    return new Error(errorMessage);
+  });
+
+  it('and then trigger an error action with the error message', (result) => {
+    expect(result).toEqual(
+      put({
+        type: 'SET_SYSTEM_MESSAGE',
+        payload: utils.app.createError(new Error(errorMessage)),
+        error: true,
+      }),
+    );
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
