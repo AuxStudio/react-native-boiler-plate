d97b450a7b6dabbf64b34e85deac836a26f1601f
Shaun Saker <shaun@aux.co.za>
2018-06-12 16:46:07 +0200

    GetDeviceLocation saga test

---
+++ sagas/location/getDeviceLocation/__tests__/getDeviceLocation.test.js
@@ -0,0 +1,113 @@
+import { call, put } from 'redux-saga/effects';
+import sagaHelper from 'redux-saga-testing';
+
+import utils from '../../../../utils';
+import getDeviceLocation from '../';
+
+const location = {
+  getDeviceLocation: jest.fn(),
+};
+
+const action = {
+  type: 'getDeviceLocation',
+};
+
+const nextAction = {
+  type: 'SUCCESS',
+};
+
+const actionWithNextAction = { ...action, meta: { nextAction } };
+
+const response = { foo: 'bar' };
+
+describe('When testing the saga without a nextAction and without a response from the api', () => {
+  const it = sagaHelper(getDeviceLocation(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(call(location.getDeviceLocation)));
+  });
+
+  // Insert test for default nextAction (if any)
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga without a nextAction and with a response from the api', () => {
+  const it = sagaHelper(getDeviceLocation(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(call(location.getDeviceLocation)));
+
+    return response;
+  });
+
+  // Insert test for default nextAction (if any)
+  it('and then trigger the SET_DEVICE_LOCATION action', (result) => {
+    expect(result).toEqual(put({ type: 'SET_DEVICE_LOCATION', payload: response }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and without a response from the api', () => {
+  const it = sagaHelper(getDeviceLocation(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(call(location.getDeviceLocation)));
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: {} }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and with a response from the api', () => {
+  const it = sagaHelper(getDeviceLocation(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(call(location.getDeviceLocation)));
+
+    return response;
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: response }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga when an error is thrown from the api', () => {
+  const it = sagaHelper(getDeviceLocation(action));
+  const errorMessage = 'Something went wrong';
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(call(location.getDeviceLocation)));
+
+    return new Error(errorMessage);
+  });
+
+  it('and then trigger an error action with the error message', (result) => {
+    expect(result).toEqual(
+      put({
+        type: 'SET_SYSTEM_MESSAGE',
+        payload: utils.app.createError(new Error(errorMessage)),
+        error: true,
+      }),
+    );
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
