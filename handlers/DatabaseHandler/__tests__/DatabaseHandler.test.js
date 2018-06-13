88cab8e773f4e1ce4f6f7e3256c6cfb2d2744c23
Shaun Saker <shaun@aux.co.za>
2018-06-13 06:15:33 +0200

    DatabaseHandler test

---
+++ handlers/DatabaseHandler/__tests__/DatabaseHandler.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { DatabaseHandler } from '../';
+
+it('renders a DatabaseHandler', () => {
+  expect(renderer.create(<DatabaseHandler dispatch={jest.fn()} authenticated />)).toMatchSnapshot();
+});
+
+it('renders a DatabaseHandler with minimum required props', () => {
+  expect(renderer.create(<DatabaseHandler dispatch={jest.fn()} />)).toMatchSnapshot();
+});
+/* eslint-enable */
