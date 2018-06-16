dca900ea9ba4408887977845aa27b56cf87e1c6a
Shaun Saker <shaun@aux.co.za>
2018-06-16 08:42:30 +0200

    Revert "Lock getTime util down to UTC"
    This reverts commit e09486c91339edeebb3e93e6c2159d03fd3853e7.

--- utils/time/getTime/__tests__/getTime.test.js
+++ utils/time/getTime/__tests__/getTime.test.js
@@ -1,8 +1,8 @@
 import getTime from '../';
 
 it('should return a time in the format, 13:00 (hh:mm) from a unix timestamp', () => {
-  expect(getTime(new Date(1527842509000))).toBe('10:41');
-  expect(getTime(new Date(1515202909000))).toBe('03:41');
-  expect(getTime(new Date(1515246109000))).toBe('15:41');
-  expect(getTime(new Date(1515271309000))).toBe('22:41');
+  expect(getTime(1527842509000)).toBe('10:41');
+  expect(getTime(1515202909000)).toBe('03:41');
+  expect(getTime(1515246109000)).toBe('15:41');
+  expect(getTime(1515271309000)).toBe('22:41');
 });
