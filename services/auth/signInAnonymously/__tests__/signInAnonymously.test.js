19cc22708c938cbdc59615ed7475b94da3386c3a
Shaun Saker <shaun@aux.co.za>
2018-06-16 15:05:08 +0200

    Added signInAnonymously test

---
+++ services/auth/signInAnonymously/__tests__/signInAnonymously.test.js
@@ -0,0 +1,23 @@
+import signInAnonymously from '../'; // eslint-disable-line
+
+jest.mock('react-native-firebase', () => {
+  return {
+    auth: () => {
+      return {
+        signInAnonymouslyAndRetrieveData: jest.fn(() => {
+          return new Promise((resolve) => {
+            resolve({
+              name: 'Shaun',
+            });
+          });
+        }),
+      };
+    },
+  };
+});
+
+it('resolves a promise', async () => {
+  expect.assertions(1);
+  const response = await signInAnonymously();
+  expect(response).toEqual({ user: { name: 'Shaun' } });
+});
