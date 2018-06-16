61f925eab37c097c924fdb6572be561cf0c74343
Shaun Saker <shaun@aux.co.za>
2018-06-16 08:22:50 +0200

    Fixed folder name of setData

---
+++ sagas/database/setData/index.js
@@ -0,0 +1,23 @@
+import { call, put } from 'redux-saga/effects';
+
+import { database } from '../../../services';
+import utils from '../../../utils';
+
+export default function* setData(action) {
+  try {
+    const response = yield call(database.setData, action.payload.ref, action.payload.data);
+    const nextAction = utils.app.prepareNextAction(action, response);
+
+    if (nextAction) {
+      yield put(nextAction);
+    }
+  } catch (error) {
+    yield put({
+      type: 'logError',
+      payload: {
+        error: utils.app.createError(error),
+        date: new Date(),
+      },
+    });
+  }
+}
