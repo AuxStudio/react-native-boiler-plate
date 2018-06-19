625fec8ba019a273b341d558432eff65cb1b2ca6
Shaun Saker <shaun@aux.co.za>
2018-06-19 18:39:04 +0200

    Make AuthHandler test more bad-ass

--- handlers/AuthHandler/__tests__/AuthHandler.test.js
+++ handlers/AuthHandler/__tests__/AuthHandler.test.js
@@ -6,3 +6,12 @@ import { AuthHandler } from '../';
 it('renders a AuthHandler', () => {
   expect(renderer.create(<AuthHandler dispatch={jest.fn()} />)).toMatchSnapshot();
 });
+
+it('dispatches getAuth on mount', () => {
+  const dispatch = jest.fn();
+
+  jest.spyOn(AuthHandler.prototype, 'componentDidMount');
+  renderer.create(<AuthHandler dispatch={dispatch} />);
+
+  expect(dispatch).toMatchSnapshot(); // dispatch function has been called
+});
