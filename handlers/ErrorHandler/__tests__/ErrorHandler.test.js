9add368d29530ba6aa6cb95c67ad917610bfcc7f
Shaun Saker <shaun@aux.co.za>
2018-06-20 16:53:01 +0200

    Update ErrorHandler tests

--- handlers/ErrorHandler/__tests__/ErrorHandler.test.js
+++ handlers/ErrorHandler/__tests__/ErrorHandler.test.js
@@ -6,50 +6,61 @@ import { ErrorHandler } from '../';
 
 jest.mock('../../../scenes/Error'); // Error scene that is displayed on error
 
-it('renders a ErrorHandler with all props', () => {
-  expect(
-    renderer.create(
-      <ErrorHandler dispatch={jest.fn()} uid="xxxx-xxxx-xxxx-xxxx">
-        <View />
-      </ErrorHandler>,
-    ),
-  ).toMatchSnapshot();
+describe('handles props', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <ErrorHandler dispatch={jest.fn()} uid="xxxx-xxxx-xxxx-xxxx">
+          <View />
+        </ErrorHandler>,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('renders with minimum required props', () => {
+    expect(
+      renderer.create(
+        <ErrorHandler dispatch={jest.fn()}>
+          <View />
+        </ErrorHandler>,
+      ),
+    ).toMatchSnapshot();
+  });
 });
 
-it('renders a ErrorHandler with minimum required props', () => {
-  expect(
-    renderer.create(
-      <ErrorHandler dispatch={jest.fn()}>
-        <View />
-      </ErrorHandler>,
-    ),
-  ).toMatchSnapshot();
-});
+describe('handles its methods', () => {
+  let spy;
+  const dispatch = jest.fn();
 
-it('catches errors in componentDidCatch', () => {
   function ProblemChild() {
     throw new Error('Error thrown from problem child');
     return <div>Error</div>; // eslint-disable-line
   }
 
-  const dispatch = jest.fn();
+  it('catches errors in componentDidCatch', () => {
+    spy = jest.spyOn(ErrorHandler.prototype, 'componentDidCatch');
 
-  const spy = jest.spyOn(ErrorHandler.prototype, 'componentDidCatch');
+    const component = renderer.create(
+      <ErrorHandler dispatch={dispatch}>
+        <ProblemChild />
+      </ErrorHandler>,
+    );
+    const instance = component.getInstance();
+    const { root } = component;
 
-  const component = renderer.create(
-    <ErrorHandler dispatch={dispatch}>
-      <ProblemChild />
-    </ErrorHandler>,
-  );
-  const instance = component.getInstance();
-  const { root } = component;
+    expect(instance.state.hasError).toBe(true);
 
-  expect(instance.state.hasError).toBe(true);
+    const errorPage = root.findByProps({ testID: 'errorPage' });
+    expect(errorPage).toBeDefined();
+    expect(spy).toHaveBeenCalled();
+    expect(dispatch).toMatchSnapshot(); // dispatch function has been called
 
-  const errorPage = root.findByProps({ testID: 'errorPage' });
-  expect(errorPage).toBeDefined();
-  expect(spy).toHaveBeenCalled();
-  expect(dispatch).toMatchSnapshot(); // dispatch function has been called
+    spy.mockClear();
+  });
 
-  spy.mockClear();
+  afterEach(() => {
+    if (spy) {
+      spy.mockReset();
+    }
+  });
 });
