9d51d7ca1de33828cd4e0393d90d647bef5bce9b
Shaun Saker <shaun@aux.co.za>
2018-06-20 16:48:32 +0200

    DatabaseHandler tests

--- handlers/DatabaseHandler/__tests__/DatabaseHandler.test.js
+++ handlers/DatabaseHandler/__tests__/DatabaseHandler.test.js
@@ -10,3 +10,30 @@ it('renders a DatabaseHandler', () => {
 it('renders a DatabaseHandler with minimum required props', () => {
   expect(renderer.create(<DatabaseHandler dispatch={jest.fn()} />)).toMatchSnapshot();
 });
+
+describe('handles its methods', () => {
+  let spy;
+  const dispatch = jest.fn();
+
+  it('dispatches listenForData on mount if authenticated', () => {
+    spy = jest.spyOn(DatabaseHandler.prototype, 'listenForData');
+
+    renderer.create(<DatabaseHandler dispatch={dispatch} authenticated />);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('does not dispatch listenForData on mount if not authenticated', () => {
+    spy = jest.spyOn(DatabaseHandler.prototype, 'listenForData');
+
+    renderer.create(<DatabaseHandler dispatch={dispatch} />);
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    if (spy) {
+      spy.mockReset();
+    }
+  });
+});
