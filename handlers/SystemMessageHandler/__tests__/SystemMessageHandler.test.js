08cdd231370ea6ba949441edf62e237c677b61be
Shaun Saker <shaun@aux.co.za>
2018-06-20 18:57:05 +0200

    Update SystemMessageHandler tests

--- handlers/SystemMessageHandler/__tests__/SystemMessageHandler.test.js
+++ handlers/SystemMessageHandler/__tests__/SystemMessageHandler.test.js
@@ -4,6 +4,13 @@ import { View } from 'react-native';
 
 import { SystemMessageHandler } from '../';
 
+jest.mock('react-native-snackbar', () => {
+  return {
+    show: jest.fn(),
+    hide: jest.fn(),
+  };
+});
+
 describe('SystemMessageHandler', () => {
   it('renders with all props', () => {
     expect(
@@ -33,3 +40,39 @@ describe('SystemMessageHandler', () => {
     ).toMatchSnapshot();
   });
 });
+
+// on update showSnackbar is called
+// after a timeout resetError is called which dispatches an action
+describe('SystemMessageHandler', () => {
+  let spy;
+  const dispatch = jest.fn();
+
+  it('shows the snackbar if systemMessage changed in componentDidUpdate', () => {
+    const component = renderer.create(
+      <SystemMessageHandler dispatch={dispatch}>
+        <View />
+      </SystemMessageHandler>,
+    );
+    const instance = component.getInstance();
+    spy = jest.spyOn(instance, 'showSnackbar');
+
+    component.update(
+      <SystemMessageHandler
+        dispatch={dispatch}
+        systemMessage={{
+          message: 'Something went wrong',
+          code: 'AUTH',
+          error: true,
+        }}
+      >
+        <View />
+      </SystemMessageHandler>,
+    );
+
+    expect(spy).toHaveBeenCalled();
+
+    // TODO: test timeout
+  });
+});
+
+// TODO: test componentDidUpdate with different message
