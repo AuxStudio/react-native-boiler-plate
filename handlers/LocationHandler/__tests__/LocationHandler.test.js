9a82b85a1dc7ce8110a1bdb9ff5a9127ebca574b
Shaun Saker <shaun@aux.co.za>
2018-06-20 17:00:17 +0200

    LocationHandler tests

--- handlers/LocationHandler/__tests__/LocationHandler.test.js
+++ handlers/LocationHandler/__tests__/LocationHandler.test.js
@@ -3,6 +3,28 @@ import renderer from 'react-test-renderer';
 
 import { LocationHandler } from '../';
 
-it('renders a LocationHandler', () => {
-  expect(renderer.create(<LocationHandler dispatch={jest.fn()} />)).toMatchSnapshot();
+describe('handles props', () => {
+  it('renders with all/minimum required props', () => {
+    expect(renderer.create(<LocationHandler dispatch={jest.fn()} />)).toMatchSnapshot();
+  });
+});
+
+describe('handles its methods', () => {
+  let spy;
+  const dispatch = jest.fn();
+
+  it('dispatches getLocationPermission on mount', () => {
+    spy = jest.spyOn(LocationHandler.prototype, 'getLocationPermission');
+
+    renderer.create(<LocationHandler dispatch={dispatch} />);
+
+    expect(spy).toHaveBeenCalled();
+    expect(dispatch).toMatchSnapshot();
+  });
+
+  afterEach(() => {
+    if (spy) {
+      spy.mockReset();
+    }
+  });
 });
