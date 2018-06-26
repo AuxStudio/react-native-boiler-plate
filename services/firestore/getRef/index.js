c5544034ac7938d1c5b8f5cce95fc14be6de91a4
Shaun Saker <shaun@aux.co.za>
2018-06-26 18:01:18 +0200

    Add firestore/getRef service

---
+++ services/firestore/getRef/index.js
@@ -0,0 +1,24 @@
+import firebase from 'react-native-firebase';
+
+import utils from '../../../utils';
+
+// Takes an array of alternating collection and document names
+// e.g. [ collection, doc, collection, doc ]
+// and returns a firestore ref
+export default function getRef(pathParts) {
+  return new Promise((resolve, reject) => {
+    let ref = firebase.firestore();
+    let isCollection = true; // always starts with a collection
+
+    try {
+      pathParts.forEach((pathPart) => {
+        ref = isCollection ? ref.collection(pathPart) : ref.doc(pathPart);
+        isCollection = !isCollection;
+      });
+
+      resolve(ref);
+    } catch (error) {
+      reject(utils.app.createError(error));
+    }
+  });
+}
