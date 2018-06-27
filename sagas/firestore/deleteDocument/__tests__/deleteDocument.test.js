bd330fac6d681c5076558f2cfd311f430f479155
Shaun Saker <shaun@aux.co.za>
2018-06-27 11:21:46 +0200

    Firestore/deleteDocument tests

---
+++ sagas/firestore/deleteDocument/__tests__/deleteDocument.test.js
@@ -0,0 +1,125 @@
+import { call, put } from 'redux-saga/effects';
+import sagaHelper from 'redux-saga-testing';
+
+import utils from '../../../../utils';
+import deleteDocument from '../';
+
+const firestore = {
+  deleteDocument: jest.fn(),
+};
+
+const action = {
+  type: 'deleteDocument',
+  meta: {
+    pathParts: ['collection', 'document'],
+  },
+};
+
+const nextAction = {
+  type: 'SUCCESS',
+};
+
+const actionWithNextAction = { ...action, meta: { ...action.meta, nextAction } };
+
+const response = { foo: 'bar' };
+
+describe('When testing the saga without a nextAction and without a response from the api', () => {
+  const it = sagaHelper(deleteDocument(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(firestore.deleteDocument, action.meta.pathParts)),
+    );
+  });
+
+  // Insert test for default nextAction (if any)
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga without a nextAction and with a response from the api', () => {
+  const it = sagaHelper(deleteDocument(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(firestore.deleteDocument, action.meta.pathParts)),
+    );
+
+    return response;
+  });
+
+  // Insert test for default nextAction (if any)
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and without a response from the api', () => {
+  const it = sagaHelper(deleteDocument(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(firestore.deleteDocument, action.meta.pathParts)),
+    );
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: {} }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and with a response from the api', () => {
+  const it = sagaHelper(deleteDocument(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(firestore.deleteDocument, action.meta.pathParts)),
+    );
+
+    return response;
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: response }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga when an error is thrown from the api', () => {
+  const it = sagaHelper(deleteDocument(action));
+  const errorMessage = 'Something went wrong';
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(call(firestore.deleteDocument, action.meta.pathParts)),
+    );
+
+    return new Error(errorMessage);
+  });
+
+  it('and then trigger an error action with the error message', (result) => {
+    expect(result).toEqual(
+      put({
+        type: 'logError',
+        payload: {
+          error: utils.app.createError(errorMessage),
+          date: new Date(),
+        },
+      }),
+    );
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
