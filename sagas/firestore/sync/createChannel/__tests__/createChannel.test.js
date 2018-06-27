9c3c545bad99dc2e03d0beca6af7aa5c00d9ed28
Shaun Saker <shaun@aux.co.za>
2018-06-27 11:58:03 +0200

    Add firestore/sync/createChannel test

---
+++ sagas/firestore/sync/createChannel/__tests__/createChannel.test.js
@@ -0,0 +1,21 @@
+import { eventChannel } from 'redux-saga';
+
+import createChannel from '../';
+
+jest.mock('../../../../../services/', () => {
+  return {
+    firestore: {
+      sync: jest.fn(),
+    },
+  };
+});
+
+it('should work', () => {
+  expect(JSON.stringify(createChannel())).toEqual(
+    JSON.stringify(
+      eventChannel(() => {
+        return () => {}; // unsubscribe function is required
+      }),
+    ),
+  );
+});
