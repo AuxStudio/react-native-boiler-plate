004b7f68be5c0b53ee397e1a9acba3a5a179d241
Shaun Saker <shaun@aux.co.za>
2018-06-27 11:36:51 +0200

    Firestore/updateDocument tests

---
+++ sagas/firestore/updateDocument/__tests__/updateDocument.test.js
@@ -0,0 +1,136 @@
+import { call, put } from 'redux-saga/effects';
+import sagaHelper from 'redux-saga-testing';
+
+import utils from '../../../../utils';
+import updateDocument from '../';
+
+const firestore = {
+  updateDocument: jest.fn(),
+};
+
+const action = {
+  type: 'updateDocument',
+  meta: {
+    pathParts: ['collection', 'document'],
+  },
+  payload: {
+    document: {
+      testing: true,
+    },
+  },
+};
+
+const nextAction = {
+  type: 'SUCCESS',
+};
+
+const actionWithNextAction = { ...action, meta: { ...action.meta, nextAction } };
+
+const response = { foo: 'bar' };
+
+describe('When testing the saga without a nextAction and without a response from the api', () => {
+  const it = sagaHelper(updateDocument(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(
+        call(firestore.updateDocument, action.meta.pathParts, action.payload.document),
+      ),
+    );
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga without a nextAction and with a response from the api', () => {
+  const it = sagaHelper(updateDocument(action));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(
+        call(firestore.updateDocument, action.meta.pathParts, action.payload.document),
+      ),
+    );
+
+    return response;
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and without a response from the api', () => {
+  const it = sagaHelper(updateDocument(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(
+        call(firestore.updateDocument, action.meta.pathParts, action.payload.document),
+      ),
+    );
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: {} }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga with a nextAction and with a response from the api', () => {
+  const it = sagaHelper(updateDocument(actionWithNextAction));
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(
+        call(firestore.updateDocument, action.meta.pathParts, action.payload.document),
+      ),
+    );
+
+    return response;
+  });
+
+  it('and then trigger an action', (result) => {
+    expect(result).toEqual(put({ ...nextAction, payload: response }));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('When testing the saga when an error is thrown from the api', () => {
+  const it = sagaHelper(updateDocument(action));
+  const errorMessage = 'Something went wrong';
+
+  it('should have called the mocked API first', (result) => {
+    expect(JSON.stringify(result)).toEqual(
+      JSON.stringify(
+        call(firestore.updateDocument, action.meta.pathParts, action.payload.document),
+      ),
+    );
+
+    return new Error(errorMessage);
+  });
+
+  it('and then trigger an error action with the error message', (result) => {
+    expect(result).toEqual(
+      put({
+        type: 'logError',
+        payload: {
+          error: utils.app.createError(errorMessage),
+          date: new Date(),
+        },
+      }),
+    );
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
