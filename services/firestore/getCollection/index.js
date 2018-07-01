202eec04f09cd847fa7d10ea4034fde0e524870b
shaunsaker <shaun@aux.co.za>
2018-07-01 08:06:53 +0200

    Firestore/getCollection service add query functionality and return data only

--- services/firestore/getCollection/index.js
+++ services/firestore/getCollection/index.js
@@ -1,15 +1,32 @@
 import utils from '../../../utils';
 import getRef from '../getRef';
 
-export default function getCollection(pathParts) {
+// Gets a collection from firestore, queries it (if query (array) provided) and
+// parses the snapshot to return docs data only
+export default function getCollection(pathParts, query) {
   return new Promise((resolve, reject) => {
     getRef(pathParts)
       .then((ref) => {
-        ref
+        let newRef;
+
+        if (query) {
+          newRef = ref.where(...query);
+        } else {
+          newRef = ref;
+        }
+
+        newRef
           .get()
           .then((collection) => {
             try {
-              resolve({ collection: collection.docs });
+              const collectionArray = collection.docs.map((document) => {
+                return {
+                  ...document.data(),
+                  id: document.id,
+                };
+              });
+
+              resolve({ collection: collectionArray });
             } catch (error) {
               if (collection.docs && true) {
                 reject(
