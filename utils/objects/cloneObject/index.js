79ddf2ebd9035c69eab7237ef30b2fcfe9c41e92
shaunsaker <shaun@aux.co.za>
2018-07-05 13:00:39 +0200

    Fix utils/cloneObject

--- utils/objects/cloneObject/index.js
+++ utils/objects/cloneObject/index.js
@@ -1,6 +1,11 @@
 // returns a copy of an object
+// FIXME
 const cloneObject = (object) => {
-  return { ...object };
+  try {
+    return JSON.parse(JSON.stringify(object)); // fails for circular objects
+  } catch (error) {
+    return { ...object }; // components don't always update with this
+  }
 };
 
 export default cloneObject;
