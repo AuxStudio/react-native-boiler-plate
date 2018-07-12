5bdd1dae2b7877981604bb50ea0d858156436ba3
shaunsaker <shaun@aux.co.za>
2018-07-12 15:03:06 +0200

    Add navigation reducers and tests

--- reducers/navigation/index.js
+++ reducers/navigation/index.js
@@ -1,4 +1,4 @@
-import { ActionConst } from 'react-native-router-flux';
+// import { ActionConst } from 'react-native-router-flux';
 import initialState from './initialState';
 import utils from '../../utils';
 
@@ -6,9 +6,31 @@ export default function navigationReducer(state = initialState, action = {}) {
   let newState;
 
   switch (action.type) {
-    case ActionConst.FOCUS:
+    // NOTE: this does not work
+    // case ActionConst.FOCUS:
+    //   newState = utils.objects.cloneObject(state);
+    //   newState.scene = action.scene; // comes from RNRF, ie. we can't control the action pattern
+    //   return newState;
+
+    case 'PUSH_PAGE':
       newState = utils.objects.cloneObject(state);
-      newState.scene = action.scene; // comes from RNRF, ie. we can't control the action pattern
+      newState.pages.push(action.payload.page);
+      return newState;
+
+    case 'POP_PAGE':
+      newState = utils.objects.cloneObject(state);
+      newState.pages.pop();
+      return newState;
+
+    case 'RESET_PAGES':
+      newState = utils.objects.cloneObject(state);
+      newState.pages = [action.payload.page];
+      return newState;
+
+    case 'REPLACE_PAGE':
+      newState = utils.objects.cloneObject(state);
+      newState.pages.pop();
+      newState.pages.push(action.payload.page);
       return newState;
 
     default:
--- reducers/navigation/initialState.js
+++ reducers/navigation/initialState.js
@@ -1,3 +1,5 @@
-const initialState = {};
+const initialState = {
+  pages: [],
+};
 
 export default initialState;
