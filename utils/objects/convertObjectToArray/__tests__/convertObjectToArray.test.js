24fac977db0226d6cd2c22ec2cad3b90f91fea8c
shaunsaker <shaun@aux.co.za>
2018-07-16 07:30:52 +0200

    Update convertObjectToArray util

--- utils/objects/convertObjectToArray/__tests__/convertObjectToArray.test.js
+++ utils/objects/convertObjectToArray/__tests__/convertObjectToArray.test.js
@@ -9,9 +9,14 @@ describe('convertObjectToArray()', () => {
     expect(convertObjectToArray({})).toEqual([]);
   });
 
-  // Should work but its not the typical use case
-  it('should work with a shallow object', () => {
-    expect(convertObjectToArray(data)).toEqual([]);
+  describe('should work with a flat object', () => {
+    it('should return an empty array', () => {
+      expect(convertObjectToArray(data)).toEqual([]);
+    });
+
+    it('should not return an empty array when second arg passed', () => {
+      expect(convertObjectToArray(data, 'name')).toEqual([{ name: 'bar', id: 'foo' }]);
+    });
   });
 
   it('should work well with a nested object', () => {
--- utils/objects/convertObjectToArray/index.js
+++ utils/objects/convertObjectToArray/index.js
@@ -1,4 +1,4 @@
-// converts an object into an array, keeping the key (as oid)
+// converts an object into an array, keeping the key (as id)
 /*
   {
     id_1: {
@@ -10,12 +10,22 @@
   }
   =>
   [{ foo: 'bar', id: 'id_1' }, { foo: 'bar', id: 'id_2' }]
+
+  where keyNameForFlatObject is the key name to create in the
+  cases where a flat object is used, e.g.
+  ({ foo: 'bar' }, 'name') => [{ name: 'bar', id: 'foo' }]
 */
 
-const convertObjectToArray = (object) => {
+const convertObjectToArray = (object, keyNameForFlatObject) => {
   let array = Object.keys(object).map((id) => {
     if (typeof object[id] === 'object') {
       return { ...object[id], id };
+    }
+    if (keyNameForFlatObject) {
+      return {
+        name: object[id],
+        id,
+      };
     }
     return null;
   });
