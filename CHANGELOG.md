271d4326a96330a7e17bf3cdff688363809d2d0f
shaunsaker <shaun@aux.co.za>
2018-10-06 11:28:28 +0200

    Updated NetworkHandler

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -41,6 +41,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Updated test snippet render block
 - Updated DatabaseHandler
 - Updated ErrorHandler
+- Updated NetworkHandler
 
 ## 1.1.2
 
--- handlers/NetworkHandler/index.js
+++ handlers/NetworkHandler/index.js
@@ -10,6 +10,7 @@ export class NetworkHandler extends React.Component {
     this.addNetInfoEventListener = this.addNetInfoEventListener.bind(this);
     this.removeNetInfoEventListener = this.removeNetInfoEventListener.bind(this);
     this.handleConnectionChange = this.handleConnectionChange.bind(this);
+    this.setNetworkConnectionInfo = this.setNetworkConnectionInfo.bind(this);
     this.disableNetwork = this.disableNetwork.bind(this);
     this.enableNetwork = this.enableNetwork.bind(this);
   }
@@ -37,14 +38,7 @@ export class NetworkHandler extends React.Component {
   }
 
   handleConnectionChange(connectionInfo) {
-    const { dispatch } = this.props;
-
-    dispatch({
-      type: 'SET_NETWORK_CONNECTION_INFO',
-      payload: {
-        network: connectionInfo,
-      },
-    });
+    this.setNetworkConnectionInfo(connectionInfo);
 
     if (
       connectionInfo.type === 'none' ||
@@ -54,6 +48,17 @@ export class NetworkHandler extends React.Component {
     } else if (connectionInfo.type !== 'none' && connectionInfo.effectiveType !== '2g') {
       this.enableNetwork();
     }
+  }
+
+  setNetworkConnectionInfo(network) {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'SET_NETWORK_CONNECTION_INFO',
+      payload: {
+        network,
+      },
+    });
   }
 
   disableNetwork() {
