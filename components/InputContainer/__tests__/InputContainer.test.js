6efc2f8387cddf5bf1e9b7e0564b78e45a0f6217
shaunsaker <shaun@aux.co.za>
2018-10-06 09:40:15 +0200

    Update InputContainer tests

--- components/InputContainer/__tests__/InputContainer.test.js
+++ components/InputContainer/__tests__/InputContainer.test.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { View } from 'react-native';
 
 import InputContainer from '..';
 
 describe('InputContainer', () => {
-  it('renders with all props', () => {
-    expect(
-      renderer.create(
-        <InputContainer
-          containerStyle={{ backgroundColor: 'red' }}
-          contentContainerStyle={{ backgroundColor: 'blue' }}
-        >
-          <View />
-        </InputContainer>,
-      ),
-    ).toMatchSnapshot();
-  });
-
   it('renders with minimum required props', () => {
     expect(renderer.create(<InputContainer />)).toMatchSnapshot();
   });
